80cdba863b1405fd34c83197f6b964f08a7b2822
hainlh <longhai230303@gmail.com>
2023-09-17 22:43:06 +0700

    Update train.py

--- train.py
+++ train.py
@@ -291,7 +291,7 @@ def get_ndcg(pred, grd, is_hit, topk):
         hit[:num_pos] = 1
         return DCG(hit, topk, devices)
 
-    device = grd.devices
+    #device = grd.devices
     pred = pred.to(devices)
     is_hit = is_hit.to(devices)
     IDCGs = torch.empty(1+topk, dtype=torch.float)
