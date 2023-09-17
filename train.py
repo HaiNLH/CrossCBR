65022516c9e68fba6b9bd64ae7c7ef94c9c9635d
hainlh <longhai230303@gmail.com>
2023-09-17 21:45:50 +0700

    Update train.py

--- train.py
+++ train.py
@@ -254,7 +254,8 @@ def get_metrics(metrics, grd, pred, topks):
     tmp = {"recall": {}, "ndcg": {}}
     for topk in topks:
         _, col_indice = torch.topk(pred, topk)
-        row_indice = torch.zeros_like(col_indice) + torch.arange(pred.shape[0], device=pred.device, dtype=torch.long).view(-1, 1)
+        row_indice = torch.zeros_like(col_indice, device=pred.device) + torch.arange(pred.shape[0], device=pred.device, dtype=torch.long).view(-1, 1)
+        
         is_hit = grd[row_indice.view(-1), col_indice.view(-1)].view(-1, topk)
 
         tmp["recall"][topk] = get_recall(pred, grd, is_hit, topk)
