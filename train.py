a2762d3f6ceb3c5832598a806afabf17a24ea82f
hainlh <longhai230303@gmail.com>
2023-09-23 22:08:52 +0700

    Update train.py

--- train.py
+++ train.py
@@ -283,27 +283,27 @@ def get_recall(pred, grd, is_hit, topk):
 
 def get_ndcg(pred, grd, is_hit, topk):
     def DCG(hit, topk, device):
-        hit = hit/torch.log2(torch.arange(2, topk+2, device=device, dtype=torch.float)).to('cuda')
+        hit = hit/torch.log2(torch.arange(2, topk+2, device=device, dtype=torch.float)).to(device)
         return hit.sum(-1)
 
     def IDCG(num_pos, topk, device):
-        hit = torch.zeros(topk, dtype=torch.float).to('cuda')
+        hit = torch.zeros(topk, dtype=torch.float).to(device)
         hit[:num_pos] = 1
         return DCG(hit, topk, device)
 
     device = pred.device
     # pred = pred.to(devices)
     # is_hit = is_hit.to(devices)
-    IDCGs = torch.empty(1+topk, dtype=torch.float, device = device).to('cuda')
+    IDCGs = torch.empty(1+topk, dtype=torch.float, device = device).to(device)
     IDCGs[0] = 1  # avoid 0/0
     for i in range(1, topk+1):
         IDCGs[i] = IDCG(i, topk, device)
 
-    num_pos = grd.sum(dim=1).clamp(0, topk).to(torch.long).to('cuda')
+    num_pos = grd.sum(dim=1).clamp(0, topk).to(torch.long).to(device)
     dcg = DCG(is_hit, topk, device)
 
     idcg = IDCGs[num_pos]
-    ndcg = dcg/idcg.to('cuda')
+    ndcg = dcg/idcg.to(device)
 
     denorm = pred.shape[0] - (num_pos == 0).sum().item()
     nomina = ndcg.sum().item()
