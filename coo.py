d3db42a11047bd0120b24c8a34644267c951b8ce
hainlh <longhai230303@gmail.com>
2023-11-24 22:43:16 +0700

    test

--- coo.py
+++ coo.py
@@ -10,16 +10,21 @@ def to_tensor(graph):
     return graph
 
 # Example COO matrix
-data = [1, 0, 1, 1, 0]
-row_indices = [0 , 0, 1, 2, 2]
-col_indices = [1, 2, 0, 1, 2]
+data = [1, 0, 1, 1, 0,1,1,0,1,1]
+row_indices = [0 , 0, 1, 2, 2, 1, 3, 2, 1, 3]
+col_indices = [1, 2, 0, 1, 0, 2, 0, 3, 3, 3]
 
-coo_graph = sp.coo_matrix((data, (row_indices, col_indices)), shape=(3, 3))
+coo_graph = sp.coo_matrix((data, (row_indices, col_indices)), shape=(4, 4))
+co_graph = coo_graph@coo_graph.T
+final = co_graph*co_graph*2
 i_count = coo_graph.sum(axis=1)
-print(i_count)
-# Convert COO matrix to PyTorch sparse tensor
-# tensor_graph = to_tensor(coo_graph).to_dense()
-test_graph = coo_graph@coo_graph.T
-print(coo_graph.todense())
-print(test_graph.todense())
+i =  final/i_count
+print(i.tocoo())
+# print(i_count)
+# print(final/i_count)
+# # Convert COO matrix to PyTorch sparse tensor
+# # tensor_graph = to_tensor(coo_graph).to_dense()
+# test_graph = coo_graph@coo_graph.T
+# print(coo_graph.todense())
+# print(test_graph.todense())
 
