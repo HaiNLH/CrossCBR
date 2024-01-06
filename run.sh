008186a0f135433c4f0477e97b822c17bc9c45dd
HaiNLH <longhai230303@gmail.com>
2024-01-06 15:32:45 +0700

    adjust

--- run.sh
+++ run.sh
@@ -1,35 +1,29 @@
-# we choose w3 w4 base on % edges in asymmatrix
-# iui filtered edges >= 20% edges in iui
-# ibi filtered edge ~ 1-2% edges in ibi
-# we not tuning much you can tuning this coeficients to get better results
+# # iFashion
+# python gene_ii_co_oc.py -d iFashion
+# python preProcess.py -d iFashion -iui 15 -ibi 2
+# python train.py -d iFashion -w3 0.8 -w4 0.8 -sw 0.9 -nw 0.1
 
-
-# Youshu
-python gene_ii_co_oc.py -d Youshu
-python preProcess.py -d Youshu -ibi 4 -iui 4
-python train.py -d Youshu -w3 0.9 -w4 0.9
+# # Youshu
+# python gene_ii_co_oc.py -d Youshu
+# python preProcess.py -d Youshu -ibi 4 -iui 4
+# python train.py -d Youshu -w3 0.9 -w4 0.9
 
 # NetEase
 python gene_ii_co_oc.py -d NetEase
 python preProcess.py -d NetEase -ibi 10 -iui 9
 python train.py -d NetEase -w3 0.8 -w4 0.8
 
-#Steam
-python gene_ii_co_oc.py -d Steam
-python preProcess.py -d Steam -ibi 10 -iui 9
-python train.py -d Steam -w3 0.8 -w4 0.8 -sw 0.8 -nw 0.2
-
 # clothing
-python gene_ii_co_oc.py -d clothing
-python preProcess.py -d clothing
-python train.py -d clothing -w3 0.1 -w4 0.1 -sw 0.8 -nw 0.2
+# python gene_ii_co_oc.py -d clothing
+# python preProcess.py -d clothing
+# python train.py -d clothing -w3 0.1 -w4 0.1 -sw 0.8 -nw 0.2
 
-# food
-python gene_ii_co_oc.py -d food
-python preProcess.py -d food -ibi 2 -iui 4
-python train.py -d food -w3 0.1 -w4 0.1 -sw 0.9 -nw 0.8
+# # food
+# python gene_ii_co_oc.py -d food
+# python preProcess.py -d food -ibi 2 -iui 4
+# python train.py -d food -w3 0.1 -w4 0.1 -sw 0.9 -nw 0.8
 
-# elctronic
-python gene_ii_co_oc.py -d electronic
-python preProcess.py -d electronic
-python train.py -d electronic -w3 0.1 -w4 0.1 -sw 0.9 -nw 0.1
+# # elctronic
+# python gene_ii_co_oc.py -d electronic
+# python preProcess.py -d electronic
+# python train.py -d electronic -w3 0.1 -w4 0.1 -sw 0.9 -nw 0.1
